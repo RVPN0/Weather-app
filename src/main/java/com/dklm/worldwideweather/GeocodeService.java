6cb122cfd31234e252541316f4268aa7d5e19e73
Lee Afeawo <afeawol0329@students.bowiestate.edu>
2024-05-13 12:57:06 -0400

    Update GeocodeService.java

--- src/main/java/com/dklm/worldwideweather/GeocodeService.java
+++ src/main/java/com/dklm/worldwideweather/GeocodeService.java
@@ -3,6 +3,7 @@ package com.dklm.worldwideweather;
 public class GeocodeService extends APIConfig {
 
     public static CoordinateData forwardGeocode(String address) {
+
         String jsonResponse = callGeocodeAPI(address, GEOCODE_API_KEY);
         CoordinateData coordinateData = new CoordinateData();
         if (jsonResponse != null) {
@@ -10,6 +11,7 @@ public class GeocodeService extends APIConfig {
             if (parseSuccess) {
                 return coordinateData; // Return the CoordinateData object directly
             } else {
+                System.out.println("Error: Failed to retrieve coordinates.");
                 return new CoordinateData("0.0", "0.0"); // Return default coordinates in case of parsing failure
             }
         } else {
